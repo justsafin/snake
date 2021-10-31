b375643fada69222821280a81e26a1210befe4fb
justsafin <justsafin@yandex.ru>
2021-10-31 23:17:59 +0300

    Function draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 3;
+            int y1 = 5;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+            Draw(5, 2, '#');
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
