6a2d200531a74a2a81e87c93ba7f25fe0ee12e0b
justsafin <justsafin@yandex.ru>
2021-11-03 23:46:59 +0300

    lists

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -8,13 +9,24 @@ namespace Snake
         {
             Point p1 = new Point(2, 5, '*');
             Point p2 = new Point(5, 2, '#');
+            Point p3 = new Point(6, 7, '&');
+            Point p4 = new Point(9, 3, '?');
 
-            p1.Draw();
-            p2.Draw();
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
+            pointList.Add(p3);
+            pointList.Add(p4);
+
+            foreach (Point i in pointList)
+            {
+                i.Draw();
+            }
 
 
             Console.ReadLine();
         }
+
 
     }
 }
