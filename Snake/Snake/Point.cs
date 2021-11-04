10644e2c6657355c978b7b9a029289fb9c9d74e6
justsafin <justsafin@yandex.ru>
2021-11-04 19:16:28 +0300

    snake game

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -9,6 +9,7 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
+        public int scr;
 
         public Point(int _x, int _y, char _sym)
         {
@@ -17,10 +18,60 @@ namespace Snake
             sym = _sym;
         }
 
+        public Point(int _x, int _y, int _scr)
+        {
+            x = _x;
+            y = _y;
+            scr = _scr;
+        }
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Directions direction)
+        {
+            if(direction == Directions.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Directions.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Directions.UP)
+            {
+                y = y - offset;
+            }
+            else if (direction == Directions.DOWN)
+            {
+                y = y + offset;
+            }
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public void DrawScr()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(scr);
+        }
+        internal bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake
 {
@@ -7,27 +8,54 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int mapWidth = 78;
+            int mapHeight = 25;
+
             Point p1 = new Point(2, 5, '*');
-            Point p2 = new Point(5, 2, '#');
-            Point p3 = new Point(6, 7, '&');
-            Point p4 = new Point(9, 3, '?');
 
-            List<Point> pointList = new List<Point>();
-            pointList.Add(p1);
-            pointList.Add(p2);
-            pointList.Add(p3);
-            pointList.Add(p4);
-
-            HorizontalLine topHline = new HorizontalLine(0, 78, 0, '-');
-            HorizontalLine botHline = new HorizontalLine(0, 78, 24, '-');
-            VerticalLine leftVline = new VerticalLine(0, 24, 0, '|');
-            VerticalLine rightVline = new VerticalLine(0, 24, 78, '|');
+            HorizontalLine topHline = new HorizontalLine(0, mapWidth, 0, '-');
+            HorizontalLine botHline = new HorizontalLine(0, mapWidth, mapHeight, '-');
+            VerticalLine leftVline = new VerticalLine(0, mapHeight, 0, '|');
+            VerticalLine rightVline = new VerticalLine(0, mapHeight, mapWidth, '|');
             
             leftVline.lineDraw();
             rightVline.lineDraw();
-
             topHline.lineDraw();
             botHline.lineDraw();
+
+            Snake snake = new Snake(p1, 5, Directions.RIGHT);
+            snake.lineDraw();
+
+            FoodCreator foodCreator = new FoodCreator(mapWidth-5, mapHeight-5, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+            
+            while(true)
+            {
+                Point head = snake.GetNextPoint();
+                if (head.x == mapWidth || head.x == 0 || head.y == mapHeight || head.y == 0 || snake.HitTail())
+                {
+                    Console.WriteLine("Вы проиграли");
+                    break;                    
+                }
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                
+            }
+
 
             Console.ReadLine();
         }
