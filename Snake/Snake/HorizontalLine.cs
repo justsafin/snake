2825ecabe0fb012cf9a37fe7efc5f60f1b144150
justsafin <justsafin@yandex.ru>
2021-11-04 15:52:06 +0300

    line classes added

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xleft, int xright, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xleft; x<=xright; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void lineDraw()
+        {
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int ytop, int ybot, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = ytop; y <= ybot; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void lineDraw()
+        {
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
